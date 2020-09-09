75fb8ef75040fded81395978d2264b1d9a7dd2e8
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-09 16:13:50 +0530

    Header component of Amazon clone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     //BEM Convention
-    <div className="app"> 
-      <h1>Hello Clever Programmers, let's build the Amazon Store 🚀</h1>
-      {/* Header */}
+    <div className="app">
+      <Header />
       {/* Home */}
     </div>
   );
