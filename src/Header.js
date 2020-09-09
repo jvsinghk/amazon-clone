92cb0e190a70098ec528e0f21eb0577285db5de1
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-09 22:33:05 +0530

    feat: added router and wip:checkout component

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,18 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt=""
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
 
       <div className="header__search">
         <input type="text" className="header__searchInput" />
