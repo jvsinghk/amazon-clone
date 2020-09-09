ae6ddf7c6480ff5e5ade36fe388c7145c0fc13aa
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-09 22:37:24 +0530

    added routes to basket to checkout page

--- src/Header.js
+++ src/Header.js
@@ -33,10 +33,13 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
-        <div className="header__optionBasket">
-          <ShoppingBasketIcon />
-          <span className="header__optionLineTwo header__basketCount">0</span>
-        </div>
+
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
