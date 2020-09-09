54d1979cef81b150960ddd57869af13f441f19c0
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-09 06:31:33 +0530

    added firbase api, removed unnecessary files and added header and home component empty files and did some code changes in app.js and removed app.css code

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA-5qnPL4AAM1KAge6opt1U7hb0SS9qFO8",
+  authDomain: "clone-4afa0.firebaseapp.com",
+  databaseURL: "https://clone-4afa0.firebaseio.com",
+  projectId: "clone-4afa0",
+  storageBucket: "clone-4afa0.appspot.com",
+  messagingSenderId: "256183782067",
+  appId: "1:256183782067:web:e43087fbf82768ad43fec6",
+  measurementId: "G-D12SY24VKQ",
+};
