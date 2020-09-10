8a9ebf4457081e8c764e7bd594db1be071dd8876
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-11 02:04:51 +0530

    feat: subtotal functionality build the subtotal to show the number of items and SUM the total price of the products and added the india ruppes instead of dollar

--- src/Home.js
+++ src/Home.js
@@ -15,15 +15,15 @@ function Home() {
       <div className="home__row">
         <Product
           id="12321341"
-          title="The lean startup"
-          price={29.99}
-          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+          title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+          price={287}
           rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
         />
         <Product
           id="49538094"
           title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
-          price={239.0}
+          price={28779}
           rating={4}
           image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
         />
@@ -31,22 +31,22 @@ function Home() {
       <div className="home__row">
         <Product
           id="4903850"
-          title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
-          price={199.99}
+          title="Fitbit Charge 2 Heart Rate + Fitness Wristband, Special Edition, Gunmetal, Large (International Version)"
+          price={14500.0}
           rating={3}
           image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
         />
         <Product
           id="23445930"
           title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-          price={98.99}
+          price={3499.0}
           rating={5}
           image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
         />
         <Product
           id="3254354345"
           title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
-          price={598.99}
+          price={11545}
           rating={4}
           image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
         />
@@ -55,7 +55,7 @@ function Home() {
         <Product
           id="90829332"
           title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor -  Super Ultra Wide Dual WQHD 5120 x1440"
-          price={1094.98}
+          price={84000}
           rating={4}
           image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
         />
