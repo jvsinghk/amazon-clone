02cce5d984583a10ef5190d3d43077c140e8620a
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-14 18:11:06 +0530

    building payment/checkout page

---
+++ src/Payment.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Payment.css";
+
+function Payment() {
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        {/* Payment Section - Delivery address  */}
+        <div className="payment__section"></div>
+        {/* Payment Section - Review Items  */}
+        <div className="payment__section"></div>
+        {/* Payment Section - Payment Method  */}
+        <div className="payment__section"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,8 +3,10 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
+  const history = useHistory();
   const [{ basket }, dispatch] = useStateValue();
   return (
     <div class="subtotal">
@@ -25,7 +27,9 @@ function Subtotal() {
         thousandSeparator={true}
         prefix={"₹"}
       />
-      <button>Proceed to Checkout</button>
+      <button onClick={(e) => history.push("/payment")}>
+        Proceed to Checkout
+      </button>
     </div>
   );
 }
